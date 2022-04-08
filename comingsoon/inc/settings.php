b3e156c4a39463c8a1a2de1b905670cfa3ce0aa8
Yogesh  Sambare <bloggingscan@gmail.com>
2022-04-08 18:41:11 +0530

    Update settings.php

--- comingsoon/inc/settings.php
+++ comingsoon/inc/settings.php
@@ -25,42 +25,48 @@ function csmp_register_menu_page() {
 }
 add_action('admin_menu', 'csmp_register_menu_page');
 
-//This function only for showing Sub-Level Administration Menu
-/* function csmp_register_menu_page() {
-    add_theme_page( 'CSMP System', 'Coming Soon Mode', 'manage_options', 'csmp-settings', 'csmp_settings_page_html', 30 );
-}
-add_action('admin_menu', 'csmp_register_menu_page'); */
-
 function csmp_plugin_settings(){
-
     // register a new section in the "wpac-setings" page
-    add_settings_section( 'csmp_label_settings_section', 'What you want to do. Just Select
-    ', 'csmp_plugin_settings_section_cb', 'csmp-settings' );
+    add_settings_section( 'csmp_label_settings_section', 'Choose Mode', 'csmp_plugin_settings_section_cb', 'csmp-settings' );
 
 }
 add_action('admin_init', 'csmp_plugin_settings');
 
 // Section 
 function csmp_plugin_settings_section_cb(){
-    echo '<div class="container">
-    <div class="field">
-      <p class="control">
-        <label class="checkbox">
-          <input type="checkbox" id="Check1" value="Value1" onclick="selectOnlyThis(this.id)" /> Live
-        </label>
-      </p>
-          <p class="control">
-        <label class="checkbox">
-          <input type="checkbox" id="Check2" value="Value1" onclick="selectOnlyThis(this.id)" /> Maintenance Mode
-        </label>
-      </p>
-      <p class="control">
-        <label class="checkbox">
-    <input type="checkbox" id="Check3" value="Value1" onclick="selectOnlyThis(this.id)" /> Coming Soon Mode
-        </label>
-      </p>
+    echo '
+    <div class="custom-select" style="width:200px;">
+    <select>
+     <option value="1">Live</option>
+      <option value="2">Coming Soon</option>
+      <option value="3">Maintainance</option>
+    </select>
+    <div>
+    <h2>Choose Page</h2>
+    <div class="custom-select" style="width:200px;">
+    <select>
+    <option value="0">Homepage</option>
+    <option value="1">About Us</option>
+    <option value="2">Contact Us</option>
+    <option value="3">Blog</option>
+    <option value="4">Review & Deals</option>
+    </select>
     </div>
-  </div>';
+    <div>
+    <h2>Who Can Access Live Site</h2>
+    <div id="list1" class="dropdown-check-list" tabindex="100">
+  <span class="anchor">Custom</span>
+  <ul class="items">
+    <li><input type="checkbox" />Administrator</li>
+    <li><input type="checkbox" />Editor</li>
+    <li><input type="checkbox" />Author</li>
+    <li><input type="checkbox" />Subscriber</li>
+  </ul>
+  
+</div>
+    </div>
+  </div> 
+    ';
 }
 
 // callback function
@@ -78,4 +84,30 @@ function csmp_deactivate_label_field_cb(){
     ?>
     <input type="text" name="csmp_deactivae_btn_label" value="<?php echo isset( $setting ) ? esc_attr( $setting ) : ''; ?>">
     <?php
-}
+} 
+
+// <?php
+// $page_titles = array();
+// $args = array(
+//     'post_type' => 'page',
+//     'posts_per_page' => -1
+// );
+
+// $the_query = new WP_Query( $args ); 
+
+// // If there are pages, let's loop
+// if($the_query->have_posts()):  
+//     while($the_query->have_posts()):
+//         $the_query->the_post(); 
+//         $page_titles[] = get_the_title();  // Add each page title to your array
+//     endwhile; 
+
+// else : 
+//     // Do stuff if no pages
+// endif;
+
+// // Display array contents
+// echo '<pre>';
+// print_r($page_titles);
+// echo '</pre>';
+// ?
