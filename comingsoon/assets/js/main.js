32fb3e350d13b03e012d8c704bb6f517f47674ae
Yogesh  Sambare <bloggingscan@gmail.com>
2022-04-13 22:30:53 +0530

    Unnecessary Things Removed

--- comingsoon/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-function selectOnlyThis(id) {
-    for (var i = 1;i <= 4; i++){
-        if ("Check" + i === id && document.getElementById("Check" + i).checked === true){
-            document.getElementById("Check" + i).checked = true;
-            } else {
-              document.getElementById("Check" + i).checked = false;
-            }
-    }  
-}
